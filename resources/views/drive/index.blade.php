8cbccf677501f05d3dfb3707d498d889394ee46c
azmigilar-gif <azmigilar@gmail.com>
2026-04-16 20:04:37 +0700

    fix: update quota usage label for clarity in drive index view

--- resources/views/drive/index.blade.php
+++ resources/views/drive/index.blade.php
@@ -46,7 +46,7 @@
                                     d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                             </svg>
                         </div>
-                        <span class="text-sm font-semibold text-gray-700">Penggunaan Kuota</span>
+                        <span class="text-sm font-semibold text-gray-700">Penggunaan</span>
                     </div>
                     <span class="text-sm font-bold text-gray-800">{{ $usedMb }} / {{ $quotaMb }} MB <span
                             class="font-normal text-gray-400">({{ $usedPct }}%)</span></span>
